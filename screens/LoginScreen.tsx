d77c631ff496fe5969130b9564100d37a9dc39a7
dantelentsoe <dllentsoe@gmail.com>
2022-08-06 22:07:32 +0200

    feat: generate toke from auth users

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,21 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { UserAuthInfo } from "../constants/types";
 import { loginUser } from "../util/auth";
 import { Alert } from "react-native";
+import { AuthContext } from "../provider/AuthProvider";
 
 function LoginScreen() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const AuthCtx = useContext(AuthContext);
 
   const loginUserHandler = async ({ email, password }: UserAuthInfo) => {
     setIsAuthenticated(true);
     try {
-      await loginUser(email, password);
+      const token = await loginUser(email, password);
+
+      AuthCtx.authenticated(token);
     } catch (error) {
       Alert.alert("AUTH error", `Auth failed due to ${error}`);
+      setIsAuthenticated(false);
     }
-    setIsAuthenticated(false);
   };
 
   if (isAuthenticated) {
--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,21 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { registerUser } from "../util/auth";
 import { Alert } from "react-native";
 import LoadingOverlay from "../components/UI/LoadingOverlay";
 import { UserAuthInfo } from "../constants/types";
+import { AuthContext } from "../provider/AuthProvider";
 
 const SignupScreen = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const AuthCtx = useContext(AuthContext);
 
   const registerUserHandler = async ({ email, password }: UserAuthInfo) => {
     setIsAuthenticated(true);
     try {
-      await registerUser(email, password);
+      const token = await registerUser(email, password);
+      AuthCtx.authenticated(token);
     } catch (error) {
       Alert.alert("AUTH error", `Registering failed due to ${error}`);
+      setIsAuthenticated(false);
     }
-    setIsAuthenticated(false);
   };
 
   if (isAuthenticated) {
