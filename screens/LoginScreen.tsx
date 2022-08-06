aa94a168bfb6fcd1e01ae227325bca6a8fec29ae
dantelentsoe <dllentsoe@gmail.com>
2022-08-06 21:28:56 +0200

    refactor: add error handling for login up

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { UserAuthInfo } from "../constants/types";
+import { loginUser } from "../util/auth";
+import { Alert } from "react-native";
 
 function LoginScreen() {
-  return <AuthContent isLogin onAuthenticate={undefined} />;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const loginUserHandler = async ({ email, password }: UserAuthInfo) => {
+    setIsAuthenticated(true);
+    try {
+      await loginUser(email, password);
+    } catch (error) {
+      Alert.alert("AUTH error", `Auth failed due to ${error}`);
+    }
+    setIsAuthenticated(false);
+  };
+
+  if (isAuthenticated) {
+    return <LoadingOverlay message="Logging in" />;
+  }
+  return <AuthContent isLogin onAuthenticate={loginUserHandler} />;
 }
 
 export default LoginScreen;
