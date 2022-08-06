f38346f3454a19a6d199a9bb6a4e4e4bd0d6b428
dantelentsoe <dllentsoe@gmail.com>
2022-08-06 17:45:17 +0200

    feat: add authentication stack

--- App.tsx
+++ App.tsx
@@ -1,25 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import ManageExpense from "./screens/ManageExpense";
+import RecentExpenses from "./screens/RecentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+import { GlobalStyles } from "./constants/styles";
+import IconButton from "./components/UI/IconButton";
+import ExpensesContextProvider from "./store/expenses-context";
 
-import ManageExpense from './screens/ManageExpense';
-import RecentExpenses from './screens/RecentExpenses';
-import AllExpenses from './screens/AllExpenses';
-import { GlobalStyles } from './constants/styles';
-import IconButton from './components/UI/IconButton';
-import ExpensesContextProvider from './store/expenses-context';
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignUpScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
+const Auth = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
+
+function AuthStack() {
+  return (
+    <Auth.Navigator
+      screenOptions={{
+        // headerStyle: { backgroundColor: "green" },
+        headerTintColor: "white",
+      }}>
+      <Auth.Screen name="Login" component={LoginScreen} />
+      <Auth.Screen name="Signup" component={SignupScreen} />
+    </Auth.Navigator>
+  );
+}
 
 function ExpensesOverview() {
   return (
     <BottomTabs.Navigator
       screenOptions={({ navigation }) => ({
         headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-        headerTintColor: 'white',
+        headerTintColor: "white",
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         tabBarActiveTintColor: GlobalStyles.colors.accent500,
         headerRight: ({ tintColor }) => (
@@ -28,18 +46,17 @@ function ExpensesOverview() {
             size={24}
             color={tintColor}
             onPress={() => {
-              navigation.navigate('ManageExpense');
+              navigation.navigate("ManageExpense");
             }}
           />
         ),
-      })}
-    >
+      })}>
       <BottomTabs.Screen
         name="RecentExpenses"
         component={RecentExpenses}
         options={{
-          title: 'Recent Expenses',
-          tabBarLabel: 'Recent',
+          title: "Recent Expenses",
+          tabBarLabel: "Recent",
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="hourglass" size={size} color={color} />
           ),
@@ -49,8 +66,8 @@ function ExpensesOverview() {
         name="AllExpenses"
         component={AllExpenses}
         options={{
-          title: 'All Expenses',
-          tabBarLabel: 'All Expenses',
+          title: "All Expenses",
+          tabBarLabel: "All Expenses",
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="calendar" size={size} color={color} />
           ),
@@ -69,9 +86,9 @@ export default function App() {
           <Stack.Navigator
             screenOptions={{
               headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-              headerTintColor: 'white',
+              headerTintColor: "white",
             }}
-          >
+            initialRouteName="login">
             <Stack.Screen
               name="ExpensesOverview"
               component={ExpensesOverview}
@@ -81,9 +98,11 @@ export default function App() {
               name="ManageExpense"
               component={ManageExpense}
               options={{
-                presentation: 'modal',
+                presentation: "modal",
               }}
             />
+
+            <Stack.Screen name="login" component={AuthStack} />
           </Stack.Navigator>
         </NavigationContainer>
       </ExpensesContextProvider>
