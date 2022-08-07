330247129e7517e98571356010e1d6c4b6649521
dantelentsoe <dllentsoe@gmail.com>
2022-08-07 11:59:06 +0200

    refactor: add types to recent expenses

--- screens/RecentExpenses.tsx
+++ screens/RecentExpenses.tsx
@@ -1,26 +1,27 @@
-import { useContext, useEffect, useState } from 'react';
+import React from "react";
+import { useContext, useEffect, useState } from "react";
 
-import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
-import ErrorOverlay from '../components/UI/ErrorOverlay';
-import LoadingOverlay from '../components/UI/LoadingOverlay';
-import { ExpensesContext } from '../store/expenses-context';
-import { getDateMinusDays } from '../util/date';
-import { fetchExpenses } from '../util/http';
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import ErrorOverlay from "../components/UI/ErrorOverlay";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { AuthContext } from "../provider/AuthProvider";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
   const [isFetching, setIsFetching] = useState(true);
-  const [error, setError] = useState();
-
+  const [error, setError] = useState<string | undefined>();
   const expensesCtx = useContext(ExpensesContext);
-
+  const authCtx = useContext(AuthContext);
   useEffect(() => {
     async function getExpenses() {
       setIsFetching(true);
       try {
-        const expenses = await fetchExpenses();
+        const expenses = await fetchExpenses(authCtx.token);
         expensesCtx.setExpenses(expenses);
       } catch (error) {
-        setError('Could not fetch expenses!');
+        setError("Could not fetch expenses!");
       }
       setIsFetching(false);
     }
