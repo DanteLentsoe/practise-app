6f6ace384dfad7f0ffed038416edef4b388f7ab9
dantelentsoe <dllentsoe@gmail.com>
2022-08-07 12:07:07 +0200

    refactor: add types to expenses list

--- components/ExpensesOutput/ExpensesList.tsx
+++ components/ExpensesOutput/ExpensesList.tsx
@@ -1,12 +1,13 @@
-import { FlatList } from 'react-native';
+import React from "react";
+import { FlatList } from "react-native";
 
-import ExpenseItem from './ExpenseItem';
+import ExpenseItem from "./ExpenseItem";
 
-function renderExpenseItem(itemData) {
+function renderExpenseItem(itemData: { item: any }) {
   return <ExpenseItem {...itemData.item} />;
 }
 
-function ExpensesList({ expenses }) {
+function ExpensesList({ expenses }: any) {
   return (
     <FlatList
       data={expenses}
