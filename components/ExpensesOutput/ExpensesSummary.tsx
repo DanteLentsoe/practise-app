7f48ce8adbfb987fb439c0dfbaae79183a107b94
dantelentsoe <dllentsoe@gmail.com>
2022-08-07 12:06:46 +0200

    refactor: add types to expense logging

--- components/ExpensesOutput/ExpensesSummary.tsx
+++ components/ExpensesOutput/ExpensesSummary.tsx
@@ -1,9 +1,10 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-import { GlobalStyles } from '../../constants/styles';
+import { GlobalStyles } from "../../constants/styles";
 
-function ExpensesSummary({ expenses, periodName }) {
-  const expensesSum = expenses.reduce((sum, expense) => {
+function ExpensesSummary({ expenses, periodName }: any) {
+  const expensesSum = expenses.reduce((sum: any, expense: { amount: any }) => {
     return sum + expense.amount;
   }, 0);
 
@@ -22,9 +23,9 @@ const styles = StyleSheet.create({
     padding: 8,
     backgroundColor: GlobalStyles.colors.primary50,
     borderRadius: 6,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   period: {
     fontSize: 12,
@@ -32,7 +33,7 @@ const styles = StyleSheet.create({
   },
   sum: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: GlobalStyles.colors.primary500,
   },
 });
